3e61052922caa944b8b394f128366d87d4e95e3d
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2026-04-25 19:25:58 -0400

    create app colors

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF13EC80);
+  static const Color primaryLight = Color(0xFF37EC13);
+  static const Color primaryExtraLight = Color(0xFFCFE7DB);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color darkBlack = Color(0xFF101B0D);
+  static const Color extraDarkBlack = Color(0xFF0D1B14);
+  static const Color mediumBlack = Color(0xFF64748B);
+  static const Color lightBlack = Color(0xFF6B7280);
+  static const Color darkGrey = Color(0xFF9CA3AF);
+}
