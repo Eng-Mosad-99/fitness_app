661bf58b6721d56a229330b9138549d15b58318f
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2026-04-25 18:47:06 -0400

    add logo_bg_image

--- lib/core/utils/assets_manager.dart
+++ lib/core/utils/assets_manager.dart
@@ -1,6 +1,7 @@
 class AppImages {
   static const String logoImage = 'assets/images/logo.png';
   static const String noNetworkImage = 'assets/images/no_network.jpg';
+  static const String loginBgImage = 'assets/images/login_bg_image.png';
 }
 
 class AppIcons {}
