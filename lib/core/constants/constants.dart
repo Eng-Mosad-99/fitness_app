718d401cedbc4c30f557e28a87c4dd52efef762c
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2026-04-25 19:15:43 -0400

    add fonts and put it in pubspec.yaml create constants class

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,5 @@
+class Constants {
+  static const String fontFamilyPlusJakartaSans = 'PlusJakartaSans';
+  static const String fontFamilyManrope = 'Manrope';
+  static const String fontFamilyBeVietnamPro = 'BeVietnamPro';
+}
--- lib/features/auth/presentation/views/login_view.dart
+++ lib/features/auth/presentation/views/login_view.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:health_fitness/core/constants/constants.dart';
+import 'package:health_fitness/core/utils/assets_manager.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Stack(
+      children: [
+        Image.asset(
+          AppImages.loginBgImage,
+          fit: BoxFit.fill,
+          height: double.infinity,
+          width: double.infinity,
+        ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Center(
+            child: Text(
+              'Login View',
+              style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontFamily: Constants.fontFamilyPlusJakartaSans,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
